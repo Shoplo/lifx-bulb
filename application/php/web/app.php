ee7e3a1418e78e168275ad3cd1be8009cfd7473b
radekb <radek.baczynski@gmail.com>
2017-09-05 19:54:02 +0200

    php pingdom

--- application/php/web/app.php
+++ application/php/web/app.php
@@ -2,4 +2,15 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-echo "HI PHP: ".date('Y-m-d H:i:s');
+use Symfony\Component\HttpFoundation\Response;
+
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+$app = new \Silex\Application();
+
+$app->get('/pingdom', function() {
+	return new Response('Hi');
+});
+
+
+$app->run($request);
